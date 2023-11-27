320f148547538a149fa716f7b14d53e458be706e
bandit.studios <banditstudiosdpd@gmail.com>
2023-11-26 22:58:13 -0500

    Implemented s3 & cloudfront for django server

--- server/comm/models.py
+++ server/comm/models.py
@@ -1,3 +1,58 @@
 from django.db import models
+from storages.backends.s3boto3 import S3Boto3Storage
 
-# Create your models here.
+class AvatarStorage(S3Boto3Storage):
+    location = 'avatars'  # Specify the S3 bucket subdirectory
+
+class User(models.Model):
+    username = models.CharField(max_length=255)
+    email = models.EmailField(unique=True)
+    uid = models.CharField(max_length=100,  unique=True)
+    bio = models.CharField(max_length=1000)
+    avatar = models.ImageField(upload_to='avatars/', null=True)
+    # Other user-related fields (e.g., profile picture, bio)
+
+    def __str__(self):
+        return self.username
+    
+class Tag(models.Model):
+    name = models.CharField(max_length=255)
+    # Other tag-related fields (e.g., description)
+
+    def __str__(self):
+        return self.name
+
+class BlogPost(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()  # Store content in a structured format (e.g., JSON)
+    created_at = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_posts')
+    tags = models.ManyToManyField(Tag)
+
+    def __str__(self):
+        return self.title
+    
+class Comment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.user.username} - {self.created_at}'
+    
+class Like(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='likes')
+    post = models.ForeignKey(BlogPost, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.user.username} likes {self.post.title}'
+    
+    
+class Follow(models.Model):
+    follower = models.ForeignKey(User, related_name='follower', on_delete=models.CASCADE)
+    following = models.ForeignKey(User, related_name='following', on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.follower.username} follows {self.following.username}'
--- server/comm/views.py
+++ server/comm/views.py
@@ -1,3 +1,33 @@
 from django.shortcuts import render
 
 # Create your views here.
+# views.py
+
+from rest_framework import viewsets
+from .models import User, Tag, BlogPost, Comment, Like, Follow
+from .serializers import UserSerializer, TagSerializer, BlogPostSerializer, CommentSerializer, LikeSerializer, FollowSerializer
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+class TagViewSet(viewsets.ModelViewSet):
+    queryset = Tag.objects.all()
+    serializer_class = TagSerializer
+
+class BlogPostViewSet(viewsets.ModelViewSet):
+    queryset = BlogPost.objects.all()
+    serializer_class = BlogPostSerializer
+
+class CommentViewSet(viewsets.ModelViewSet):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+
+class LikeViewSet(viewsets.ModelViewSet):
+    queryset = Like.objects.all()
+    serializer_class = LikeSerializer
+
+class FollowViewSet(viewsets.ModelViewSet):
+    queryset = Follow.objects.all()
+    serializer_class = FollowSerializer
+
